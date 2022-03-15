f465faaadcde62f813aab215415c49d0d93874f5
Lucas Guillerot lguiller <lguiller@student.42.fr>
2022-03-15 16:10:43 +0100

    suppression d'un sum imbrique, creation des range x et y avec min et max (ca marche tres bien), calcule des neighbors dans un parfor

--- Life.m
+++ Life.m
@@ -22,28 +22,28 @@ classdef Life < handle
                 obj.board(randi(obj.size(2)), randi(obj.size(1))) = 1;
             end
             prevBoard = obj.board;
-            for y = 1:obj.size(2)
-                for x = 1:obj.size(1)
-                    neighbors = getNeighbors(obj, prevBoard, x, y);
+            newBoard = obj.board;
+            w = obj.size(1);
+            h = obj.size(2);
+            parfor y = 1:h
+                for x = 1:w
+                    neighbors = getNeighbors(prevBoard, x, y, w, h);
                     if (prevBoard(y, x) == 1 && (neighbors < 2 || neighbors > 3))
-                        obj.board(y, x) = 0;
+                        newBoard(y, x) = 0;
                     elseif (prevBoard(y, x) == 0 && neighbors == 3)
-                        obj.board(y, x) = 1;
+                        newBoard(y, x) = 1;
                     end
                 end
             end
+            obj.board = newBoard;
         end
     end
 end
 
-function neighbors = getNeighbors(obj, board, x, y)
-    yRange = y - 1:y + 1;
-    xRange = x - 1:x + 1;
-    yRange = yRange(yRange > 0);
-    yRange = yRange(yRange <= obj.size(2));
-    xRange = xRange(xRange > 0);
-    xRange = xRange(xRange <= obj.size(1));
-    neighbors = sum(sum(board(yRange, xRange)));
+function neighbors = getNeighbors(board, x, y, w, h)
+    yRange = max(y - 1, 1):min(y + 1, h);
+    xRange = max(x - 1, 1):min(x + 1, w);
+    neighbors = sum(board(yRange, xRange), [1, 2]);
     if (board(y, x) == 1)
         neighbors = neighbors - 1;
     end
